b87cd576e8d35d2968e14449a1de42d8c74e3c40
leongziqian <118972199+leongziqian@users.noreply.github.com>
2024-09-17 14:57:42 +0800

    Update Space_News_Articles_Clustering.py

--- Space_News_Articles_Clustering.py
+++ Space_News_Articles_Clustering.py
@@ -53,15 +53,15 @@ if page == "Home":
     st.title("Space News Articles Clustering")
 
     def autoplay_audio(file_path: str):
-    with open(file_path, "rb") as f:
-        data = f.read()
-        b64 = base64.b64encode(data).decode()
-        md = f"""
-            <audio id="themeAudio" autoplay="true" style="display:none;">
-            <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
-            </audio>
-            """
-        st.markdown(md, unsafe_allow_html=True)
+        with open(file_path, "rb") as f:
+            data = f.read()
+            b64 = base64.b64encode(data).decode()
+            md = f"""
+                <audio id="themeAudio" autoplay="true" style="display:none;">
+                <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+                </audio>
+                """
+            st.markdown(md, unsafe_allow_html=True)
 
     # Add autoplay audio
     audio_url = 'starwars.mp3'
